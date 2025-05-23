2fb582768fb4b2fd92b2752ae4fddd45e1d2119a
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-05-23 15:39:14 +0530

    nedd morew change sin home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Header from '../Header'
 
-import homeappliences from '../../images/homeappliences.png'
+import homeappliences from '../../images/homeappliences1.png'
 import mobilesandlaptops from '../../images/mobilesandlaptops.jpg'
+import kichenone from '../../images/kichen2.png'
+import electronic from '../../images/electronic.jpg'
+import fashion from '../../images/fashion.jpg'
+import sports from '../../images/sports2.png'
+import food from '../../images/food.png'
+
+import { FaChevronRight } from "react-icons/fa";
+
+
 
 import './index.css'
 
@@ -15,6 +24,30 @@ const majorCategories = [{
   id:2,
   name:"Mobiles And Laptops",
   image:mobilesandlaptops,
+},
+{
+  id:3,
+  name:"Kichen Appliences",
+  image:kichenone,
+},
+{
+  id:4,
+  name:"Electronics",
+  image:electronic,
+},
+{
+  id:5,
+  name:"Fashion And Clothing",
+  image:fashion
+},
+{
+  id:6,
+  name:"Sports and Gym",
+  image:sports,
+},{
+  id:7,
+  name:"Food and Snacks",
+  image:food,
 }
 
 ]
@@ -31,8 +64,13 @@ const Home = () => {
                 <h1>{each.name}</h1>
               </li> )
             }
+            <FaChevronRight className='right-arrow-topics' />
           </ul>
+          <div>
+            <div></div>
+          </div>
        </div>
+
     </div>
   )
 }
